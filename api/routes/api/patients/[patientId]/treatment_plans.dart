3bf7fa73f970af95f94764a6efe22423ae7cf0e7
r2 <ahmadolukotun@gmail.com>
2025-08-07 21:05:47 +0100

    added possible routes

---
+++ api/routes/api/patients/[patientId]/treatment_plans.dart
@@ -0,0 +1,9 @@
+import 'package:dart_frog/dart_frog.dart';
+
+Response onRequest(
+  RequestContext context,
+  String patientId,
+) {
+  // TODO: implement route handler
+  return Response(body: 'This is a new route!');
+}
